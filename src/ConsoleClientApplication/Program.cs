ae495f6d0bc3d15e570a5e37729852c6f475dcbf
Christophe Geers <geersch@gmail.com>
2014-06-01 09:08:02 +0200

    Added ignore file, source code and documentation

---
+++ src/ConsoleClientApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO.Ports;
+using CGeers.Cardfon;
+
+namespace ConsoleClientApplication
+{
+    class Program
+    {
+        static void Main()
+        {            
+            using (VerifoneOmni3750 omniTerminal = new VerifoneOmni3750 { TerminalId = 1234 })
+            {
+                // Set the communication settings of the serial port.
+                omniTerminal.SerialPort.BaudRate = 9600;
+                omniTerminal.SerialPort.Parity = Parity.Even;
+                omniTerminal.SerialPort.DataBits = 7;
+                omniTerminal.SerialPort.StopBits = StopBits.One;
+
+                // Establish a connection to the serial port (COM1).
+                omniTerminal.SerialPort.Open(5);                                
+                omniTerminal.SendTransactionRequest(39.95, 101, "EUR");
+            }            
+
+            Console.ReadLine();
+        }
+    }
+}
